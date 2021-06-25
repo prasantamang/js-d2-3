cfebbfe118bee432b4bd748bf6923fafce938924
prasantamang <prashantamang059@gmail.com>
2021-06-25 12:17:59 +0545

    newfile

---
+++ script.js
@@ -0,0 +1,29 @@
+//pagewrapper
+const PageWrapper = document .createElement('div');
+document.body.appendChild(PageWrapper);
+
+//creating input 
+const FirstInput =document.createElement('input');
+PageWrapper.appendChild(FirstInput);
+
+//creating add button
+const AddButton = document.createElement('button');
+PageWrapper.appendChild(AddButton);
+AddButton.innerHTML="Add"
+
+//creting list
+const Ul = document.createElement('ul');
+PageWrapper.appendChild(Ul);
+
+//creating listen eventer
+AddButton.addEventListener('click',READ_WRITE);
+
+//creating funtion
+function READ_WRITE(){
+    const Inside_list =document.createElement('li');
+    Ul.appendChild(Inside_list);
+    Inside_list.innerHTML = FirstInput.value
+}
+//console.log
+FirstInput.value=""
+
